065e71d1d4c3c5e73925ecc47c7b5072ebb87954
Valentin Longchamp <valentin@longchamp.me>
2022-01-02 15:18:11 +0100

    part 2 for day8

--- day8/day8.py
+++ day8/day8.py
@@ -25,6 +25,65 @@ def find_unique_segments(input: typing.List) -> int:
     return num
 
 
+def update_translator_first_pass(digit: str, trans: typing.Dict) -> None:
+    if len(digit) == 2:
+        trans['1'] = digit
+    elif len(digit) == 3:
+        trans['7'] = digit
+    elif len(digit) == 4:
+        trans['4'] = digit
+    elif len(digit) == 7:
+        trans['8'] = digit
+
+
+def update_translator_second_pass(digit: str, trans: typing.Dict) -> None:
+    if len(digit) == 6:
+        # 0, 6, 9
+        if set(trans['4']).issubset(set(digit)):
+            trans['9'] = digit
+        elif set(trans['1']).issubset(set(digit)):
+            trans['0'] = digit
+        else:
+            # 6
+            trans['6'] = digit
+
+
+def update_translator_third_pass(digit: str, trans: typing.Dict) -> None:
+    if len(digit) == 5:
+        # 2, 3, 5
+        if set(trans['1']).issubset(set(digit)):
+            trans['3'] = digit
+        elif set(digit).issubset(set(trans['9'])):
+            trans['5'] = digit
+        else:
+            trans['2'] = digit
+
+
+def decode(input: typing.List, trans: typing.Dict) -> int:
+    dec = ''
+    for d in input:
+        for k,v in trans.items():
+            if set(d) == set(v):
+                dec += k
+    return int(dec)
+
+def decode_lines(signals: typing.List, outputs: typing.List, debug: bool) -> int:
+    output_values = []
+    for signal, output in zip(signals, outputs):
+        line = signal + output
+        translator = {}
+        for digit in line:
+            update_translator_first_pass(digit, translator)
+        for digit in line:
+            update_translator_second_pass(digit, translator)
+        for digit in line:
+            update_translator_third_pass(digit, translator)
+        if debug:
+            print(translator)
+        output_values.append(decode(output, translator))
+    return sum(output_values)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Solve day8 exercise')
     parser.add_argument('file', type=pathlib.Path, help='input file')
@@ -34,3 +93,4 @@ if __name__ == '__main__':
 
     signals, outputs = read_input(args.file)
     print(find_unique_segments(outputs))
+    print(decode_lines(signals, outputs, args.debug))
